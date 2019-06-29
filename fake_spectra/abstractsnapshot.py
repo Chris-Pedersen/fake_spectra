2bd6a2c0fe3b5a54cbdde5724b161986eaf0cb8d
Chris-Pedersen <christian.pedersen.17@ucl.ac.uk>
2019-06-29 11:37:36 +0100

    Velocities hack temp fix

--- fake_spectra/abstractsnapshot.py
+++ fake_spectra/abstractsnapshot.py
@@ -300,6 +300,13 @@ class BigFileSnapshot(AbstractSnapshot):
                     print("Rescaling temperature by: ", self.Tscale)
                 ie*=self.Tscale
                 return ie
+            ## Hack to compensate for missing units in bigfiles
+            ## see https://github.com/MP-Gadget/MP-Gadget/issues/295
+            elif blockname=="Velocity":
+                (start, end) = self._segment_to_partlist(part_type = part_type, segment=segment)
+                velocities=np.array(self._f_handle[str(part_type)+"/"+blockname][start:end])
+                velocities*=(self.get_header_attr("Time"))**(-3./2.) # rescale by a^{-3/2}
+                return velocities
             else:
                 (start, end) = self._segment_to_partlist(part_type = part_type, segment=segment)
                 return self._f_handle[str(part_type)+"/"+blockname][start:end]
